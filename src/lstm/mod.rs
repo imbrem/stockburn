04b9971120e5ea7f129e33aca0903380e0231eea
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-26 16:34:46 -0400

    Stock LSTM

--- src/lstm/mod.rs
+++ src/lstm/mod.rs
@@ -2,10 +2,16 @@
 The LSTM implementation: a rather direct translation of https://gitlab.com/tekne/stock-lstm
 */
 
-use tch::nn::{VarStore, Sequential};
+use tch::nn::{self, Linear, VarStore, LSTM};
 
 /// The StockLSTM model from https://gitlab.com/tekne/stock-lstm
-pub type StockLSTM = Sequential;
+#[derive(Debug)]
+pub struct StockLSTM {
+    /// This model's LSTM layers
+    pub lstm_layers: Vec<LSTM>,
+    /// This model's linear layer
+    pub linear_layer: Linear,
+}
 
 /// A descriptor for an instance of the StockLSTM model
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
@@ -15,7 +21,7 @@ pub struct StockLSTMBuilder {
     /// The number of stock prices to predict
     pub stocks: usize,
     /// The hidden LSTM layers in use
-    pub hidden: Vec<usize>
+    pub hidden: Vec<usize>,
 }
 
 impl StockLSTMBuilder {
@@ -29,11 +35,22 @@ impl StockLSTMBuilder {
         StockLSTMBuilder {
             inputs,
             stocks,
-            hidden: vec![Self::DEFAULT_HIDDEN; Self::DEFAULT_NO_HIDDEN]
+            hidden: vec![Self::DEFAULT_HIDDEN; Self::DEFAULT_NO_HIDDEN],
         }
     }
     /// Build a `StockLSTM` over a given `VarStore`
-    pub fn build(&self, _vs: &VarStore) -> StockLSTM {
-        unimplemented!()
+    pub fn build(&self, vs: &VarStore) -> StockLSTM {
+        let mut lstm_layers = Vec::with_capacity(self.hidden.len());
+        let mut input_size = self.inputs;
+        for hidden in self.hidden.iter().copied() {
+            let lstm = nn::lstm(&vs.root(), input_size as i64, hidden as i64, Default::default());
+            lstm_layers.push(lstm);
+            input_size = hidden;
+        }
+        let linear_layer = nn::linear(&vs.root(), input_size as i64, self.stocks as i64, Default::default());
+        StockLSTM {
+            lstm_layers,
+            linear_layer
+        }
     }
 }
