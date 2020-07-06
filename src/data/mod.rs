ba00a248fbfed0da6671be318e32de0849118f94
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-07-06 12:26:03 -0400

    Added clock set

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -2,10 +2,11 @@
 Data processing and IO functions
 */
 use crate::*;
-use chrono::{DateTime, Utc};
-use num::NumCast;
+use chrono::{DateTime, Duration, NaiveDate, Utc};
+use num::{Float, NumCast};
 use serde::{Deserialize, Serialize};
 use ta::{Close, High, Low, Open, Volume};
+use util::to_ns;
 
 pub mod fake;
 pub mod polygon;
@@ -56,6 +57,38 @@ where
         Prediction {
             c: self.c,
             v: self.v,
+        }
+    }
+}
+
+/// Push clock, with a period measured in seconds / 2 pi
+pub fn push_clock_period<F>(period: F, time: DateTime<Utc>, dest: &mut Vec<F>)
+where
+    F: Float + Copy,
+{
+    let naive = time.naive_utc();
+    let naive_0 = NaiveDate::from_ymd(2020, 0, 0).and_hms(0, 0, 0);
+    let dt = naive - naive_0;
+    let dt: F = to_ns(dt);
+    let scaled_dt = dt / period;
+    let (sin_dt, cos_dt) = scaled_dt.sin_cos();
+    dest.push(sin_dt);
+    dest.push(cos_dt);
+}
+
+/// Push a set of clocks, with duration periods
+pub fn clocks<'a, F>(
+    durations: &'a [Duration],
+) -> impl FnMut(DateTime<Utc>, &mut Vec<F>) + Send + Sync + Copy + 'a
+where
+    F: Float + Copy + NumCast,
+{
+    move |time, dest| {
+        for duration in durations.iter() {
+            let tau: F = NumCast::from(2.0 * std::f64::consts::PI).expect("Pi fits in F");
+            let duration_ns: F = to_ns(*duration);
+            let period = duration_ns / tau;
+            push_clock_period(period, time, dest)
         }
     }
 }
@@ -137,7 +170,6 @@ where
         self.v.into()
     }
 }
-
 
 impl Prediction {
     /// The number of fields a neural network must predict to yield a tick prediction
